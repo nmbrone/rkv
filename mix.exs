071f53e73adb11d5456369a06bf5a9a9c7923397
Serhii Snozyk <serhii.snozyk@gmail.com>
2026-01-22 15:57:37 +0100

    chore: prepare automated release (#9)

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule Rkv.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/nmbrone/rkv"
+
   def project do
     [
       app: :rkv,
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Rkv",
+      source_url: @source_url,
+      homepage_url: @source_url,
+      description: description(),
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -20,7 +28,27 @@ defmodule Rkv.MixProject do
 
   defp deps do
     [
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.40", only: :dev, runtime: false, warn_if_outdated: true}
+    ]
+  end
+
+  defp description do
+    "A simple ETS-based key-value storage with the ability to watch changes."
+  end
+
+  defp package do
+    [
+      maintainers: ["Serhii Snozyk"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Rkv",
+      extras: ["CHANGELOG.md"]
     ]
   end
 end
